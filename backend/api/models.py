8c2e54e15c9cafbab076e70a4415aa5a46265d0c
Jack Deye <jackdeye@ucla.edu>
2025-04-26 10:45:04 -0700

    Cleaned up db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,5 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.db.models import CompositePrimaryKey
 
 
 class CountyCurrent(models.Model):
@@ -23,7 +17,7 @@ class CountyCurrent(models.Model):
 
 class StateTimeseries(models.Model):
     state_territory = models.TextField(db_column="State/Territory")
-    week_ending_date = models.TextField(db_column="Week_Ending_Date")
+    week_ending_date = models.DateField(db_column="Week_Ending_Date")
     data_collection_period = models.TextField(db_column="Data_Collection_Period")
     state_territory_wval = models.FloatField(db_column="State/Territory_WVAL")
     national_wval = models.FloatField(db_column="National_WVAL")
@@ -33,4 +27,4 @@ class StateTimeseries(models.Model):
 
     class Meta:
         db_table = "state_timeseries"
-        unique_together = [["state_territory", "week_ending_date"]]
+        pk = CompositePrimaryKey("state_territory", "week_ending_date")
