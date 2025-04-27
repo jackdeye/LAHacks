6fe127014a6f035f9a5130fc086e0d01b6819716
Jack Deye <jackdeye@ucla.edu>
2025-04-27 03:35:08 -0700

    Some stuff

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("county", views.get_county),
     path("predictions", views.get_predictions, name="get_predictions"),
     path("force_email", views.force_email),
+    path("predictions", views.get_predictions, name="get_predictions"),
 ]
