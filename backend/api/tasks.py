572f2cf14aa1610fe751f2cf16fce69903cf606b
Diam0ndz <29808458+Diam0ndz@users.noreply.github.com>
2025-04-27 04:47:23 -0800

    Prediction email

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,5 +1,6 @@
-from .models import CountyCurrent, StateTimeseries, EmailList
+from .models import CountyCurrent, StateTimeseries, EmailList, FuturePrediction
 from django.core.mail import send_mail
+from django.forms.models import model_to_dict
 
 # Send email if:
 # - If wcat is High+
@@ -67,7 +68,29 @@ def send_email():
                 create_email = True
                 email_reason = 3
         
-        if create_email:
+        if create_email == False and latest_cat not in 'Very Low':
+            prediction = model_to_dict(FuturePrediction.objects.filter(state=state).first())
+            print(prediction)
+            pred1 = prediction['week_1_prediction']
+            pred2 = prediction['week_2_prediction']
+            pred3 = prediction['week_3_prediction']
+            pred4 = prediction['week_4_prediction']
+
+            if pred1 and abs(pred1 - latest_wval) / latest_wval > 0.325:
+                create_email = True
+                email_reason = 4
+            elif pred2 and abs(pred2 - latest_wval) / latest_wval > 0.325:
+                create_email = True
+                email_reason = 4
+            elif pred3 and abs(pred3 - latest_wval) / latest_wval > 0.325:
+                create_email = True
+                email_reason = 4
+            elif pred4 and abs(pred4 - latest_wval) / latest_wval > 0.325:
+                create_email = True
+                email_reason = 4
+
+
+        if create_email and email_reason <= 3:
             print(f"Sending emails for {state}:")
             email_body = f"""\
 Hello.
@@ -83,6 +106,52 @@ Current Week - {latest_record.ending_date}
 Last Week - {previous_record.ending_date}
 
 Based on these values, we recommend taking extra caution in avoiding COVID.
+Wearing a mask outdoors, social distancing, and making sure you're vaccinated are 
+all recommended steps you can take. 
+
+Please visit the following World Health Organization site to learn more:
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public
+
+Thank you for staying informed."""
+
+            print(email_body)
+
+            emails = EmailList.objects.filter(
+                            location=state
+            )
+
+            for email in list(emails.values()):
+                print(email)
+
+                send_mail(
+                    'Recent COVID Trends',
+                    email_body,
+                    'noreply@wastewatchers.com',
+                    [email['email']],
+                    fail_silently=False,
+                )
+        elif create_email and email_reason >= 4:
+            print(f"Sending emails for {state}:")
+            email_body = f"""\
+Hello.
+We are emailing you in regard to COVID risk in {state}.
+
+The current week's CDC WVal Category is {latest_cat}.
+The CDC WVal Category for last week was {previous_cat}.
+
+The current week's CDC WVal is {latest_wval}.
+The CDC WVal for last week is {previous_wval}.
+
+Using an AI model, we are able to predict the potential future CDC WVal.
+The predicted CDC WVal one week from now is {pred1}.
+The predicted CDC WVal two week from now is {pred2}.
+The predicted CDC WVal three week from now is {pred3}.
+The predicted CDC WVal four week from now is {pred4}.
+
+Current Week - {latest_record.ending_date}
+Last Week - {previous_record.ending_date}
+
+Based on these predicted values, we recommend taking extra caution in avoiding COVID.
 Wearing a mask outdoors, social distancing, and making sure you're vaccinated are 
 all recommended steps you can take. 
 
