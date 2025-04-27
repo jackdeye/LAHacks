66ce9a7984f3fef363999588fe2120517f85bc06
23clarej <23clarej@students.harker.org>
2025-04-27 02:28:17 -0700

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import StateMap from "./components/StateMap.jsx";
-import Banner from "./components/Banner.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Banner from "./components/Banner";
+import StateMap from "./components/StateMap";
+import Notify from "./components/Notify";
+import Information from "./components/Information";
 import "./App.css";
 
 function App() {
   return (
-    <div style={{ textAlign: "center" }}>
-      {/* <Banner /> */}
-      <StateMap />
-    </div>
+    <Router>
+      <Banner />
+      <Routes>
+        <Route path="/" element={<StateMap />} />
+        <Route path="/signup" element={<Notify />} />
+        <Route path="/info" element={<Information />} />
+      </Routes>
+    </Router>
   );
 }
 
